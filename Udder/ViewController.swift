6fd7875fd21933bf7670f62ec04ab655256dd916
DozeStrawberry <101461681+DozeStrawberry@users.noreply.github.com>
2022-06-23 10:58:38 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by liu ya yun on 2022/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
